04dd64cc23ff733925b0d5f094d6c7f16ab8315b
Tamar Kalandarishvili <tamarkalandarishvili@Tamars-Mac-mini.local>
2023-08-21 21:50:40 -0700

    first commit

--- index.js
+++ index.js
@@ -39,3 +39,22 @@ let alphabetString = 'abcdefghijklmnopqrstuvwxyz';
  * 
  * 
  * ↓ YOUR CODE HERE ↓ */
+
+
+
+
+
+
+// Question 2: alphabetArray
+console.log(`--------------------------
+Question 2: alphabetArray \n`);
+
+let alphabetArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+/*
+ * Using the provided code above:
+ * Step 1: Reorganize the alphabet array from z to a (See documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse)
+ * Step 2: Convert the elements of the array into a string (with no spaces between the letters) and print it to the console (See documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join)
+ * 
+ * 
+ * ↓ YOUR CODE HERE ↓ */
