bda236c37ad460ace6b5368fe35fc2ca9a8850d6
Tamar Kalandarishvili <tamarkalandarishvili@Tamars-Mac-mini.local>
2023-08-21 21:44:58 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+/*
+   Copyright (c) 2023 Promineo Tech
+   Author:  Promineo Tech Academic Team
+   Subject:  Arrays and Functions Lab
+   FE Lab 03
+*/
+
+/* ----------------------------------------------------- */
+
+/* Key Terms:
+ * Element
+ * Array 
+ * Built-in methods
+ * Parameter 
+ * Argument 
+ * 
+ *                                              
+ * Objectives: 
+ * Apply various built-in array methods
+ * Define a function using proper syntax 
+ * 
+*/
+
+/* ----------------------------------------------------- */
+// Please do not alter the existing code unless instructed to do so. 
+// Read the comments and add your code where it is specified for each question. 
+/* ----------------------------------------------------- */
+
+/*-------------------------------------------------------*/
+// Question 1: alphabetString
+console.log(`--------------------------
+Question 1: alphabetString \n`);
+
+let alphabetString = 'abcdefghijklmnopqrstuvwxyz'; 
+
+/*
+ * Using the provided code above:
+ * Step 1: Convert the alphabetString to an array and print it to the console (See documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split)
+ * 
+ * 
+ * ↓ YOUR CODE HERE ↓ */
